c063bb4d7c55674e5a42d7271d2187561f5d641c
KAT <katreadwell1@gmail.com>
2020-05-03 00:11:43 -0700

    friends.js setup

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,172 @@
 //save app data as an array of objects in below format:
-
+[
 {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Bowser",
+    "photo":"https://www.mariowiki.com/images/8/82/BowserNSMBUDX.png",
+    "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+
+  {
+    "name":"Princess Peach",
+    "photo":"https://www.mariowiki.com/images/b/b7/SuperMarioParty_Peach_2.png",
     "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+
+  {
+    "name":"Mario",
+    "photo":"https://www.mariowiki.com/images/2/25/Mario_New_Super_Mario_Bros_U_Deluxe.png",
+    "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+
+  {
+    "name":"Luigi",
+    "photo":"https://www.mariowiki.com/images/e/e1/Luigi_New_Super_Mario_Bros_U_Deluxe.png",
+    "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+
+  {
+    "name":"Yoshi",
+    "photo":"https://www.mariowiki.com/images/4/4d/Yoshi_-_Mario_Party_10.png",
+    "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+
+  {
+    "name":"Toad",
+    "photo":"https://www.mariowiki.com/images/d/db/SMR_Toad.png",
+    "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+
+  {
+    "name":"Wario",
+    "photo":"https://www.mariowiki.com/images/2/29/SMPWario.png",
+    "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+
+  {
+    "name":"Waluigi",
+    "photo":"https://www.mariowiki.com/images/2/27/SuperMarioParty_Waluigi.png",
+    "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+
+  {
+    "name":"Piranha",
+    "photo":"https://www.mariowiki.com/images/d/d1/NSMBU_Piranha_Plant_in_Pipe_Artwork.png",
+    "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+
+  {
+    "name":"Princess Daisy",
+    "photo":"https://www.mariowiki.com/images/9/90/MKT_Artwork_Daisy.png",
+    "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
       ]
   }
-  
+]
